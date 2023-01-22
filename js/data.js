0f252e7d9ca1964b84eaaed500ac5ea6b2fb0185
Rasulwebs <boboqulovrasul80@gmail.com>
2023-01-22 12:00:02 +0500

    data added

---
+++ js/data.js
@@ -0,0 +1,211 @@
+import roomImg1 from "../images3/romIm1.jpg";
+import roomImg2 from "../images3/roomImg2.jpg";
+import roomImg3 from "../images3/roomImg3.jpg";
+import roomImg4 from "../images3/roomImg4.jpg";
+import roomImg5 from "../images3/roomImg5.jpg";
+import roomImg6 from "../images3/roomImg6.jpg";
+import prImg1 from "../images3/prImg1.png";
+import prImg2 from "../images3/prImg2.png";
+import prImg3 from "../images3/prImg3.png";
+import prImg4 from "../images3/prImg4.png";
+
+let db = [
+  {
+    roomImg: roomImg1,
+    categoryTitle: "Bedroom",
+    prImg: prImg1,
+    prTitle: "Armchair",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+  {
+    roomImg: roomImg2,
+    categoryTitle: "Bedroom",
+    prImg: prImg2,
+    prTitle: "Premium Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+  {
+    roomImg: roomImg3,
+    categoryTitle: "Bedroom",
+    prImg: prImg3,
+    prTitle: "Minimal Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+  {
+    roomImg: roomImg4,
+    categoryTitle: "Bedroom",
+    prImg: prImg4,
+    prTitle: "Dining Chair",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+  {
+    roomImg: roomImg5,
+    categoryTitle: "Bedroom",
+    prImg: prImg2,
+    prTitle: "Premium Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+  {
+    roomImg: roomImg6,
+    categoryTitle: "Bedroom",
+    prImg: prImg3,
+    prTitle: "Minimal Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+
+  //   ==========================================================================Dinning room
+
+  {
+    roomImg: roomImg2,
+    categoryTitle: "Dinning Room",
+    prImg: prImg2,
+    prTitle: "Premium Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+
+  {
+    roomImg: roomImg4,
+    categoryTitle: "Dinning Room",
+    prImg: prImg4,
+    prTitle: "Dining Chair",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+
+  {
+    roomImg: roomImg6,
+    categoryTitle: "Dinning Room",
+    prImg: prImg3,
+    prTitle: "Minimal Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+
+  //   =======================================meeteng room
+
+  {
+    roomImg: roomImg1,
+    categoryTitle: "Dinning Room",
+    prImg: prImg1,
+    prTitle: "Armchair",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+  {
+    roomImg: roomImg2,
+    categoryTitle: "Dinning Room",
+    prImg: prImg2,
+    prTitle: "Premium Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+
+  {
+    roomImg: roomImg4,
+    categoryTitle: "Dinning Room",
+    prImg: prImg4,
+    prTitle: "Dining Chair",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+
+  // ===================================================================workspace room
+
+  {
+    roomImg: roomImg2,
+    categoryTitle: "Workspace",
+    prImg: prImg2,
+    prTitle: "Premium Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+  {
+    roomImg: roomImg3,
+    categoryTitle: "Workspace",
+    prImg: prImg3,
+    prTitle: "Minimal Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+  {
+    roomImg: roomImg4,
+    categoryTitle: "Workspace",
+    prImg: prImg4,
+    prTitle: "Dining Chair",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+  {
+    roomImg: roomImg5,
+    categoryTitle: "Workspace",
+    prImg: prImg2,
+    prTitle: "Premium Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+
+  // ==============================living
+
+  {
+    roomImg: roomImg4,
+    categoryTitle: "Living Room",
+    prImg: prImg4,
+    prTitle: "Dining Chair",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+
+  {
+    roomImg: roomImg6,
+    categoryTitle: "Living Room",
+    prImg: prImg3,
+    prTitle: "Minimal Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+
+  //   ==================kitchen
+
+  {
+    roomImg: roomImg1,
+    categoryTitle: "Kitchen",
+    prImg: prImg1,
+    prTitle: "Armchair",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+  {
+    roomImg: roomImg2,
+    categoryTitle: "Kitchen",
+    prImg: prImg2,
+    prTitle: "Premium Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+
+  // ====================================================living sapce
+
+  {
+    roomImg: roomImg3,
+    categoryTitle: "Living Space",
+    prImg: prImg3,
+    prTitle: "Minimal Sofa",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+  {
+    roomImg: roomImg4,
+    categoryTitle: "Living Space",
+    prImg: prImg4,
+    prTitle: "Dining Chair",
+    prDesc: "Light single chair",
+    prPrice: "$145",
+  },
+];
